f26fe759c65f7099096f9a6a16b24bcc1257ce5b
Tyran <me@tyran.ru>
2020-05-02 18:18:46 +0300

    Получилось!

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from django.core.paginator import Paginator
 import csv
 import urllib.parse
-from pprint import pprint
 
 with open(BUS_STATION_CSV, newline='', encoding='cp1251') as csvfile:
     all_stations = csv.DictReader(csvfile)
@@ -22,25 +21,17 @@ def bus_stations(request):
     print(f'current_page равен {current_page}')
     articles = paginator.get_page(current_page)
     print(f'articles равен {articles}')
+    prev_page_number = next_page_number = 0
     if articles.has_previous():
         prev_page_number = int(current_page)-1
     if articles.has_next():
         next_page_number = int(current_page)+1
     prev_page_url = urllib.parse.urlencode({'page' : prev_page_number})
     next_page_url = urllib.parse.urlencode({'page': next_page_number})
-    prev_page_url = 'bus_stations?'+str(prev_page_url)
-    next_page_url = 'bus_stations?'+str(next_page_url)
-    data = paginator.page(current_page)
-    pprint(data.object_list)
-    stations = station = {}
-    for each in data.object_list:
-        station['Name'] = each['Name']
-        station['Street'] = each['Street']
-        station['District'] = each['District']
-        pprint(station)
-
+    prev_page_url = reverse('bus_stations') + '?' + str(prev_page_url)
+    next_page_url = reverse('bus_stations') + '?' + str(next_page_url)
     return render(None, 'index.html', context={
-        'bus_stations': [{'Name': 'Имя', 'Street': 'Улица', 'District': 'Район'}],
+        'bus_stations': articles,
         'current_page': current_page,
         'prev_page_url': prev_page_url,
         'next_page_url': next_page_url,
