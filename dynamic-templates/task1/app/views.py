e47474e1e1246a6cffc7fc7e1af2b3f2df8d7963
Tyran <me@tyran.ru>
2020-05-03 23:29:26 +0300

    Задача 3. Смог передать слоаврь (получив его из списка). Но не помогло)

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -11,21 +11,21 @@ with open(FILE_CSV, newline='', encoding='utf_8_sig') as csvfile:
     data_list = csv.reader(csvfile, delimiter=';')
     data_list = list(data_list)
 
-    # for each in data_list:
-    #     data = {}
-    #     data.update('Год': each[0])
-    #
-    #
-    #     pprint(each)
-
 
 def inflation_view(request):
     template_name = 'app/inflation.html'
     context = {}
-    with open(FILE_CSV, newline='', encoding='utf_8_sig') as csvfile:
-        data_dict = csv.DictReader(csvfile, delimiter=';')
-        for each in data_dict:
-            context.update(each)
-        # pprint(context)
-        # print(type(context))
-        return render(request, template_name, context)
+    temp_dict = {}
+    # with open(FILE_CSV, newline='', encoding='utf_8_sig') as csvfile:
+    #     data_dict = csv.DictReader(csvfile, delimiter=';')
+    #     for each in data_dict:
+    #         id = each['Год']
+    #         temp_dict.update(id: each)
+    #         context.update[{each['Год']}: temp_dict]
+    #     pprint(context)
+    #     print(type(context))
+    for year in data_list:
+        id = year[0]
+        context[id] = year
+    pprint(context)
+    return render(request, template_name, context)
