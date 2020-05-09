fa0017077717e81a819d5727eb4f62a1156e0344
Tyran <me@tyran.ru>
2020-05-09 19:44:36 +0300

    Задача 5. Часть 2

--- databases_2/orm_migrations/school/admin.py
+++ databases_2/orm_migrations/school/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
 
-from .models import Student, Teacher
+from .models import Student, Teacher, School
+
+
+# @admin.register(Student)
+# class StudentAdmin(admin.ModelAdmin):
+#     pass
+#
+#
+# @admin.register(Teacher)
+# class TeacherAdmin(admin.ModelAdmin):
+#     pass
+
+class SchoolInline(admin.TabularInline):
+    model = School
+    extra = 1
 
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    pass
+    inlines = (SchoolInline,)
 
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    pass
+    inlines = (SchoolInline,)
--- databases_2/orm_migrations/school/models.py
+++ databases_2/orm_migrations/school/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Teacher(models.Model):
     name = models.CharField(max_length=30, verbose_name='Имя')
     subject = models.CharField(max_length=10, verbose_name='Предмет')
+    students = models.ManyToManyField('Student', related_name = 'students', through='School')
 
     class Meta:
         verbose_name = 'Учитель'
@@ -15,8 +16,8 @@ class Teacher(models.Model):
 
 class Student(models.Model):
     name = models.CharField(max_length=30, verbose_name='Имя')
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
     group = models.CharField(max_length=10, verbose_name='Класс')
+    teachers = models.ManyToManyField(Teacher, related_name = 'teachers', through='School')
 
     class Meta:
         verbose_name = 'Ученик'
@@ -24,3 +25,11 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class School(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    #
+    # def __str__(self):
+    #     return '{0}_{1}'.format(self.name, self.name)
