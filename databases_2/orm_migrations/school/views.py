659c7dabaacb3e7e4852c621b0ec3052989d3c63
Tyran <me@tyran.ru>
2020-05-10 21:17:14 +0300

    Задача 5. Часть 1. Вынес главный тег на первое место!

--- databases_2/orm_migrations/school/views.py
+++ databases_2/orm_migrations/school/views.py
@@ -7,8 +7,9 @@ from .models import Student, Teacher
 def students_list(request):
     template = 'school/students_list.html'
     students = Student.objects.all().order_by('group')
-    print(students)
-    context = {'object_list': students}
+    teachers = Teacher.objects.all()
+    print(students, teachers)
+    context = {'object_list': students, 'teachers_list': teachers}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
