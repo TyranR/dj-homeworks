bab3a295d1e326132563918035bb6d09d12d72e2
Tyran <me@tyran.ru>
2020-05-10 17:29:40 +0300

    Задача 5. Часть 1. Исправления в покраске тегов

--- databases_2/m2m_relations/articles/views.py
+++ databases_2/m2m_relations/articles/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView
 from django.shortcuts import render
+from pprint import pprint
 
 from .models import Article, Badge, Relation
 
@@ -7,8 +8,8 @@ from .models import Article, Badge, Relation
 def articles_list(request):
     template = 'articles/news.html'
     articles = Article.objects.all().order_by('-published_at')
-    context = {'object_list': articles}
-
+    related = Relation.objects.all()
+    context = {'object_list': articles, 'related': related}
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
